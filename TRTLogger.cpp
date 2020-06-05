3147679e85b12871610d100d145d102f68267342
zzc <local@local.com>
2020-06-04 22:56:47 -0700

    added TRT logger

---
+++ TRTLogger.cpp
@@ -0,0 +1,8 @@
+//
+//  TRTLogger.cpp
+//  
+//
+//  Created by zzc on 6/4/20.
+//
+
+#include "TRTLogger.hpp"
---
+++ TRTLogger.hpp
@@ -0,0 +1,13 @@
+//
+//  TRTLogger.hpp
+//  
+//
+//  Created by zzc on 6/4/20.
+//
+
+#ifndef TRTLogger_hpp
+#define TRTLogger_hpp
+
+#include <stdio.h>
+
+#endif /* TRTLogger_hpp */
