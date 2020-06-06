f25d24b5cda90ecead53385a8f08aae22770736b
zzc <local@local.com>
2020-06-06 16:55:12 -0700

    add imageUtilies

---
+++ imageUtilies.hpp
@@ -0,0 +1,16 @@
+// Simple TensorRT logger class
+
+#ifndef TRTLogger_hpp
+#define TRTLogger_hpp
+
+#include "NvInfer.h"
+#include <iostream>
+
+class tensorRTLogger: public nvinfer1::ILogger {
+public:
+    void log(Severity severity, const char* msg) override {
+        std::cout << "TRT: " << msg << std::endl;
+    }
+};
+
+#endif /* TRTLogger_hpp */
