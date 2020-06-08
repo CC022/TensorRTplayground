6b486338cae15a998d90ca8b49570399f7bbc1a2
cc022 <local@local.com>
2020-06-07 17:28:26 -0700

    neoInit

--- TRTLogger.hpp
+++ TRTLogger.hpp
@@ -11,6 +11,7 @@ public:
     void log(Severity severity, const char* msg) override {
         std::cout << "TRT: " << msg << std::endl;
     }
+    // NeoLoggerTest
 };
 
 #endif /* TRTLogger_hpp */
